a65d80e09542432ef0034b31f8fd12f262857563
Daniel G <code@danielg0.com>
2025-05-06 17:37:34 +0100

    Add script to export weights of all clusters

---
+++ experiments/1_Variance/simpoints/collectWeights.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Output csv headers
+echo benchmark,interval,weight,cluster
+for p in $(find * -maxdepth 1 -type d)
+do
+	cd $p
+	for weights in $(find * -maxdepth 1 -type f -name "*.weights")
+	do
+		cat $weights | awk "{print \"$p,\" \"${weights/.weights/},\" \$1 \",\" \$2;}"
+	done
+	cd ..
+done
+
+exit
+
+# Find every experiment in this directory
+for p in $(find * -maxdepth 3 -mindepth 3 -type d)
+do
+	count=$(grep "switch_cpus\.cpi" $p/stats.txt | wc -l)
+	if [ $count -ne 2 ]; then
+		# in no-warmup case
+		count_nw=$(grep "system.cpu.cpi" $p/stats.txt | wc -l)
+		if [ $count -eq 0 -a $count_nw -eq 1 ]; then
+			cpi=$(grep "system.cpu.cpi" $p/stats.txt | awk '{print $2;}')
+		else
+			echo ERROR: %p
+			exit -1
+		fi
+	else
+		cpi=$(grep "switch_cpus\.cpi" $p/stats.txt | tail -n 1 | awk '{print $2;}')
+	fi
+
+	echo ${p////,},$cpi
+done
