e722c7c0e516a8092c1b1d9015711c01f10a5ba4
Daniel G <code@danielg0.com>
2025-05-27 14:50:54 +0100

    Varying interval size for coremark zip

---
+++ experiments/3_coremarkzip/collectLabels.py
@@ -0,0 +1,95 @@
+#!/bin/python3
+
+# collect the N closest simpoints to a centre for each group
+
+import argparse
+import pathlib
+import re
+import sys
+
+parser = argparse.ArgumentParser(
+        prog = "SimPoint Gatherer",
+        description = "Gather the N points closest to each SimPoint cluster's centre"
+)
+
+parser.add_argument(
+        "-N",
+        type = int,
+        default = 10,
+        help = "Number of points to pick for each cluster",
+)
+
+parser.add_argument(
+        "-inLabels",
+        type = argparse.FileType("r"),
+        required = True,
+)
+
+parser.add_argument(
+        "-inWeights",
+        type = argparse.FileType("r"),
+        required = True,
+)
+
+parser.add_argument(
+        "-outPoints",
+        type = argparse.FileType("w"),
+        required = True,
+)
+
+parser.add_argument(
+        "-outWeights",
+        type = argparse.FileType("w"),
+        required = True,
+)
+
+args = parser.parse_args()
+# map of cluster to its weight
+weights = {}
+# map of arrays: cluster -> [(distance, bbv index)]
+points = {}
+
+weight = re.compile(r"^([+\-e.0-9]+) (\d+)$")
+# gather cluster weights
+for line in args.inWeights.readlines():
+    match = weight.match(line)
+    if match is None:
+        print("Error: couldn't parse weight '" + line + "'")
+        sys.exit(-1)
+    else:
+        weighting = float(match.group(1))
+        cluster = int(match.group(2))
+        weights[cluster] = weighting
+
+# regex to match a integer and float (might be of form 0.43e+34)
+label = re.compile(r"^(\d+) ([+\-e.0-9]+)$")
+# track index of current line
+i = 0
+for line in args.inLabels.readlines():
+    match = label.match(line)
+    if match is None:
+        print("Warning: Didn't match line '" + line + "'")
+        continue
+    else:
+        cluster = int(match.group(1))
+        distance = float(match.group(2))
+
+        if cluster in points:
+            points[cluster].append((distance, i))
+        else:
+            points[cluster] = [(distance, i)]
+        i += 1
+
+# output 10 closest points for each cluster
+for key in sorted(points):
+    sorted_points = sorted(points[key])
+    if len(sorted_points) < args.N:
+        print("Warning: Less than N points for cluster " + str(key))
+
+    candidates = min(args.N, len(sorted_points))
+    point_weight = weights[key] / candidates
+    for i in range(candidates):
+        distance, index = sorted_points[i]
+        point_name = str(key) + "." + str(i)
+        args.outPoints.write(str(index) + " " + point_name + "\n")
+        args.outWeights.write(str(point_weight) + " " + point_name + "\n")
---
+++ experiments/3_coremarkzip/runCheckpoints.py
@@ -0,0 +1,140 @@
+#!/bin/python3
+
+# Python script that parses a simpoint points file, and runs all the
+# corresponding checkpoints from a directory
+
+import argparse
+import os
+import pathlib
+import re
+import sys
+import subprocess
+import multiprocessing
+
+parser = argparse.ArgumentParser(
+        prog = "Checkpoint Runner",
+        description = "Given a simpoint points file, run all the corresponding checkpoints"
+)
+
+parser.add_argument(
+        "--inPoints",
+        type = argparse.FileType("r"),
+        required = True,
+)
+
+parser.add_argument(
+        "--checkpointdir",
+        type = pathlib.Path,
+        required = True,
+)
+
+parser.add_argument(
+        "--outdir",
+        type = pathlib.Path,
+        required = True,
+)
+
+parser.add_argument(
+        "--warmup",
+        type = int,
+        required = True,
+)
+
+parser.add_argument(
+        "--width",
+        type = int,
+        required = True,
+)
+
+parser.add_argument(
+        "gem5cmd",
+        type = str,
+        nargs = "+",
+)
+
+args = parser.parse_args()
+print(args)
+
+# traverse point list, determing mapping from simpoint id to checkpoint
+simpoints = []
+simpoint_line = re.compile(r"^(\d+) (\d+)\.(\d+)$")
+simpoint_id = re.compile(r".+simpoint_(\d+)_.+")
+for simpoint in args.inPoints.readlines():
+    match = simpoint_line.match(simpoint)
+    if match is None:
+        print("Couldn't match line in in points: '" + simpoint + "'")
+        sys.exit(-1)
+    else:
+        offset = int(match.group(1))
+        cluster = match.group(2)
+        index = match.group(3)
+
+        # attempt to find matching unique checkpoint
+        inst = str(max((offset * args.width) - args.warmup, 0))
+        search = [f for f in args.checkpointdir.glob("cpt.*_inst_" + inst + "_*")]
+        if len(search) == 0:
+            print("Couldn't find checkpoint for: '" + simpoint + "'")
+            sys.exit(-1)
+        if len(search) > 1:
+            print("Found more than one checkpoint: " + str(search))
+            sys.exit(-1)
+        checkpoint = search[0]
+
+        # extract checkpoint id
+        # make sure to add one
+        match = simpoint_id.match(checkpoint.name)
+        if match is None:
+            print("Couldn't find id in '" + checkpoint.name + "'")
+            sys.exit(-1)
+        else:
+            cpt_id = str(1 + int(match.group(1)))
+
+            # checkpoints with no warmup won't work, handle them manually
+            no_warmup = (offset * args.width) < args.warmup
+
+            simpoints.append((cluster, index, cpt_id, no_warmup))
+
+# method to measure a metric given a (cluster, index, cpt) tuple
+def run_checkpoint(config):
+    # increase oom score
+    oom_score = open(f"/proc/{os.getpid()}/oom_score_adj", "w")
+    oom_score.write("500")
+    oom_score.close()
+
+    cluster, index, cpt, no_warmup = config
+
+    # create directory for output
+    resultdir = args.outdir / cluster / index
+    resultdir.mkdir(parents = True, exist_ok = True)
+
+    # form command
+    command = list(tuple(args.gem5cmd))
+    command.insert(1, "--outdir")
+    command.insert(2, str(resultdir))
+    # if there's no warmup, run from the beginning manually as gem5 breaks on an
+    # empty warmup
+    if no_warmup:
+        command.extend([
+            "--maxinsts", str(args.width),
+        ])
+    else:
+        command.extend([
+            "--restore-simpoint-checkpoint",
+            "-r", cpt,
+            "--checkpoint-dir", str(args.checkpointdir),
+        ])
+
+    with open(resultdir / "log.log", "w") as log:
+        with open(resultdir / "err.log", "w") as err:
+            print(command)
+
+            process = subprocess.Popen(command, stdout=log, stderr=err)
+            status = process.wait()
+
+            if status != 0:
+                print("Exited with status code " + str(status))
+
+# create a set of jobs to measure metrics for each checkpoint
+pool = multiprocessing.Pool(1)
+with pool:
+    pool.map(run_checkpoint, simpoints)
