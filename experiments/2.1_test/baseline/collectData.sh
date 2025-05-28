db77e932401065f4532fc90a2b7467fb815d1ba1
Daniel G <code@danielg0.com>
2025-05-28 18:02:14 +0100

    Also collect IPC

--- experiments/2.1_test/baseline/collectData.sh
+++ experiments/2.1_test/baseline/collectData.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Output csv headers
-echo benchmark,real_cpi
+echo benchmark,real_cpi,real_ipc
 
 # Find every benchmark in this directory
 for bench in $(find * -maxdepth 0 -type d)
 do
 	cpi=$(grep "system.cpu.cpi" ./$bench/stats.txt | tail -n 1 | awk '{print $2;}')
-	echo $bench,$cpi
+	ipc=$(grep "system.cpu.ipc" ./$bench/stats.txt | tail -n 1 | awk '{print $2;}')
+	echo $bench,$cpi,$ipc
 done
--- experiments/2.1_test/results/collectData.sh
+++ experiments/2.1_test/results/collectData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output csv headers
-echo benchmark,interval,cluster,index,cpi
+echo benchmark,interval,cluster,index,cpi,ipc
 
 # Find every experiment in this directory
 for p in $(find * -maxdepth 3 -mindepth 3 -type d)
@@ -12,13 +12,16 @@ do
 		count_nw=$(grep "system.cpu.cpi" $p/stats.txt | wc -l)
 		if [ $count -eq 0 -a $count_nw -eq 1 ]; then
 			cpi=$(grep "system.cpu.cpi" $p/stats.txt | awk '{print $2;}')
+			ipc=$(grep "system.cpu.ipc" $p/stats.txt | awk '{print $2;}')
 		else
 			echo ERROR: $p > /dev/stderr
 			cpi=
+			ipc=
 		fi
 	else
 		cpi=$(grep "switch_cpus\.cpi" $p/stats.txt | tail -n 1 | awk '{print $2;}')
+		ipc=$(grep "switch_cpus\.ipc" $p/stats.txt | tail -n 1 | awk '{print $2;}')
 	fi
 
-	echo ${p////,},$cpi
+	echo ${p////,},$cpi,$ipc
 done
