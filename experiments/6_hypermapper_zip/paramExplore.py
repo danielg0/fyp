137f44ccd2b67c20d680119d8b8cc4048cffeb50
Daniel G <code@danielg0.com>
2025-06-09 22:32:13 +0100

    Finish hypermapper

--- experiments/6_hypermapper_zip/paramExplore.py
+++ experiments/6_hypermapper_zip/paramExplore.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# picked as its 60GB/8GB
+THREADS=7
+
 import argparse
 import hypermapper
 import os
@@ -7,19 +10,28 @@ import pathlib
 import re
 import subprocess
 import tempfile
+import time
+import multiprocessing
 
 # command arguments for all the paths we need
 parser = argparse.ArgumentParser(
 	prog = "BO SimPoint Experiment",
 	description = "Optimise hardware parameters using SimPoint sets",
 )
-# csv data out
+# csv data out & text log (to supplement hypermapper)
 parser.add_argument(
 	"--data-out",
 	type = argparse.FileType("w"),
 	required = True,
 	help = "File to write out CSV statistics to",
 )
+parser.add_argument(
+	"--log",
+	type = argparse.FileType("w"),
+	required = True,
+	help = "Supplemental logfile",
+)
+
 # gem5 running
 parser.add_argument(
 	"--gem5-binary",
@@ -42,7 +54,8 @@ parser.add_argument(
 parser.add_argument(
 	"--gem5-checkpoints",
 	type = pathlib.Path,
-	required = True
+	required = True,
+	help = "A folder containing folders for each interval, that then contain checkpoints"
 )
 parser.add_argument(
 	"--weightfile",
@@ -86,7 +99,7 @@ parser.add_argument(
 )
 
 args = parser.parse_args()
-print(args)
+args.log.write(str(args) + "\n")
 
 # parse target intervals and weights file
 # greatest interval given used as baseline
@@ -99,17 +112,19 @@ for inWeight in args.weightfile:
 		weights[interval] = {}
 		weightfile = open(weightpath, "r")
 		for simpoint in weightfile:
-			weight_s, cpt_id = simpoint.split(" ")
+			weight_s, cpt_id_s = simpoint.split(" ")
 			weight = float(weight_s)
+			cpt_id = int(cpt_id_s)
 			weights[interval][cpt_id] = weight
 	except:
-		print("Got error parsing while processing weight '" + inWeight + "'")
+		args.log.write("Got error parsing while processing weight '" + inWeight + "'\n")
 baseline = max(weights.keys())
 
 # run gem5 once with given parameters and then pass through mcpat to get power/area
 # params is a dict with the fields:
 #   "cpt_id": checkpoint id to run
 #           : if negative, no warmup, and absolute value is interval to simulate for
+#		^ note this is currently never the case
 #   "cpt_dir": checkpoint directory to run
 #
 #   "rob_size": size of reorder buffer
@@ -169,7 +184,7 @@ def run_gem5_mcpat(params):
 			gem5_command.extend(["--maxinsts", str(abs(params["cpt_id"]))])
 	
 		# execute gem5
-		print(" ".join(gem5_command))
+		args.log.write(" ".join(gem5_command) + "\n")
 		with open(tmpdirname + "/gem5/log.log", "w") as log:
 			with open(tmpdirname + "/gem5/err.log", "w") as err:
 				process = subprocess.Popen(gem5_command, stdout=log, stderr=err)
@@ -185,7 +200,7 @@ def run_gem5_mcpat(params):
 			"--config", tmpdirname + "/gem5/config.json",
 			"--output", tmpdirname + "/mcpat-in.xml",
 		]
-		print(" ".join(convert))
+		args.log.write(" ".join(convert) + "\n")
 		with open(tmpdirname + "/gem5tomcpat.log", "w") as log:
 			with open(tmpdirname + "/gem5tomcpat.err", "w") as err:
 				process = subprocess.Popen(convert, stdout=log, stderr=err)
@@ -203,7 +218,7 @@ def run_gem5_mcpat(params):
 			"-print_level", "1",
 			"-opt_for_clk", "1"
 		]
-		print(" ".join(mcpat_command))
+		args.log.write(" ".join(mcpat_command) + "\n")
 		with open(tmpdirname + "/mcpat.out", "w") as out:
 			with open(tmpdirname + "/mcpat.err", "w") as err:
 				process = subprocess.Popen(mcpat_command, stdout=out, stderr=err)
@@ -258,15 +273,99 @@ def run_gem5_mcpat(params):
 		energy = ((1000000000 * simtime) * (subthreshold_leakage + gate_leakage + runtime_dynamic)) / instructions
 		return {"energy": energy, "performance": cpi, "time": (time_gem5 + time_mcpat)}
 
+# run the jobs for a single simpoint set and calculate the weighted average single-threaded
+# also calculates the baseline for this configuration too
+# takes a dict with these fields
+#   "rob_size": size of reorder buffer
+#   "lsq_size": size of load/store queues
+#   "p_width": pipeline width
+#   "interval": interval width to target
+# returned is None or a dict with the fields:
+#   "energy": energy used in nanojoules/instruction
+#   "performance": in cpi (cycles/instruction)
+#   "time": total user cpu time spent processing
+#   "energy_baseline": energy value of the baseline
+#   "performance_baseline": performance ""
+def run_simpoint_set(params):
+	# run each checkpoint
+	checkpoint_dir = args.gem5_checkpoints / str(params["interval"])
+	# map to gather results in
+	result = {
+		"energy": 0,
+		"performance": 0,
+		"time": 0,
+	}
+	for cpt_id, weight in weights[params["interval"]].items():
+		subrun = {
+			"cpt_id": cpt_id,
+			"cpt_dir": checkpoint_dir,
+			"rob_size": params["rob_size"],
+			"lsq_size": params["lsq_size"],
+			"p_width": params["p_width"],
+		}
+
+		subresult = run_gem5_mcpat(subrun)
+		if subresult == None:
+			return None
+
+		result["energy"] += (subresult["energy"] * weight)
+		result["performance"] += (subresult["performance"] * weight)
+		result["time"] += subresult["time"]
+
+	# do we need to run the baseline too?
+	if params["interval"] == baseline:
+		result["performance_baseline"] = performance
+		result["energy_baseline"] = energy
+	else:
+		baseline_params = dict(params)
+		baseline_params["interval"] = baseline
+		baseline_stats = run_simpoint_set(baseline_params)
+		result["performance_baseline"] = baseline_stats["performance"]
+		result["energy_baseline"] = baseline_stats["energy"]
+
+	return result
+
 # compute multiple simpoint metrics in parallel with passed in parameters, whilst logging process to a .csv
 # takes a dictionary:
 #   "rob_size": (array) size of reorder buffer
 #   "lsq_size": (array) size of load/store queues
 #   "p_width": (array) pipeline width
-#   "interval_width": (array) interval width to use
+#   "interval": (array) interval width to use (this will always only be one value)
 def run_parallel_gem5(base_params):
-	print(base_params)
-	exp_count = len(base_params["rob_size"])
-	return {"energy": [0] * exp_count, "performance": [0] * exp_count}
+	args.log.write(str(base_params) + "\n")
+	args.log.flush()
 
+	exp_count = len(base_params)
+
+	# unfold map of arrays into array of maps
+	experiments = [{
+		"rob_size": base_params["rob_size"][i],
+		"lsq_size": base_params["lsq_size"][i],
+		"p_width": base_params["p_width"][i],
+		"interval": base_params["interval"][i],
+	} for i in range(exp_count)]
+
+	# get ordering value for reconstruction of best-found so far in R
+	order = time.monotonic_ns()
+	pool = multiprocessing.Pool(THREADS)
+	result = None
+	with pool:
+		result = pool.map(run_simpoint_set, experiments)
+
+	# log calculations to csv and refold array of maps into map of arrays
+	# refold array of maps into map of arrays
+	hypermapper_result = {
+		"energy": [],
+		"performance": [],
+	}
+	for r in result:
+		args.data_out.write(f'{order},{r["rob_size"]},{r["lsq_size"]},{r["p_width"]},{r["energy"]},{r["performance"]},{r["time"]},{r["energy_baseline"]},{r["performance_baseline"]}\n')
+		hypermapper_result["energy"].append(r["energy"])
+		hypermapper_result["performance"].append(r["performance"])
+	args.data_out.flush()
+
+	return hypermapper_result
+
+args.data_out.write("order,rob_size,lsq_size,p_width,energy,performance,time,energy_baseline,performance_baseline\n")
+args.data_out.flush()
 hypermapper.optimizer.optimize("scenario.json", run_parallel_gem5)
--- experiments/6_hypermapper_zip/run.sh
+++ experiments/6_hypermapper_zip/run.sh
@@ -11,5 +11,5 @@ GEM5_MEMORY="--mem-size=8GiB --caches --l2cache --l1d_size=64KiB --l1i_size=64Ki
 	--gem5-checkpoints ./in/ \
 	--gem5tomcpat ./gem5tomcpat.py --gem5tomcpat-template-warm ./template_switchcpu_x86.xml --gem5tomcpat-template-cold ./template_cold_x86.xml \
 	--weightfile 125000,./in/125000.weights 16000000,./in/16000000.weights \
-	--mcpat ~/mcpat/mcpat --data-out results.csv \
+	--mcpat ~/mcpat/mcpat --data-out results.csv --log log.txt \
 	-- -c ./bin/zip --options='$COREMARK_ARGS' --cpu-type=O3CPU $GEM5_MEMORY
