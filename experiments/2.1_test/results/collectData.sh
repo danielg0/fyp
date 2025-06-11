9040fb7e595475964f2eab091be7cf9a80f78313
Daniel G <code@danielg0.com>
2025-06-11 01:28:18 +0100

    x256 get more timing

--- experiments/2.1_test/results/collectData.sh
+++ experiments/2.1_test/results/collectData.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Output csv headers
-echo benchmark,interval,cluster,index,cpi,ipc
+echo benchmark,interval,cluster,index,cpi,ipc,timing
 
 # Find every experiment in this directory
 for p in $(find * -maxdepth 3 -mindepth 3 -type d)
@@ -14,14 +14,14 @@ do
 			cpi=$(grep "system.cpu.cpi" $p/stats.txt | awk '{print $2;}')
 			ipc=$(grep "system.cpu.ipc" $p/stats.txt | awk '{print $2;}')
 		else
-			echo ERROR: $p > /dev/stderr
-			cpi=
-			ipc=
+			echo ERROR: %p
+			exit -1
 		fi
 	else
 		cpi=$(grep "switch_cpus\.cpi" $p/stats.txt | tail -n 1 | awk '{print $2;}')
 		ipc=$(grep "switch_cpus\.ipc" $p/stats.txt | tail -n 1 | awk '{print $2;}')
 	fi
+	timing=$(grep -Po "\d+\.\d+(?=user)" $p/simulation.time)
 
-	echo ${p////,},$cpi,$ipc
+	echo ${p////,},$cpi,$ipc,$timing
 done
