869e56414d014b4a5bd7d81eb6883598362091be
Daniel G <code@danielg0.com>
2025-06-12 23:12:35 +0100

    Add support for 4000000 weights

--- experiments/6_hypermapper_zip/run.sh
+++ experiments/6_hypermapper_zip/run.sh
@@ -10,6 +10,6 @@ GEM5_MEMORY="--mem-size=8GiB --caches --l2cache --l1d_size=64KiB --l1i_size=64Ki
 ./paramExplore.py --gem5-binary $GEM5 --gem5-configfile $GEM5_CONFIG_FILE \
 	--gem5-checkpoints ./in/ \
 	--gem5tomcpat ./gem5tomcpat.py --gem5tomcpat-template-warm ./template_switchcpu_x86.xml --gem5tomcpat-template-cold ./template_cold_x86.xml \
-	--weightfile 125000,./in/125000.weights 16000000,./in/16000000.weights \
+	--weightfile 125000,./in/125000.weights 16000000,./in/16000000.weights 4000000,./in/4000000.weights \
 	--mcpat ~/mcpat/mcpat --data-out results.csv --log log.txt \
 	-- -c ./bin/zip --options="$COREMARK_ARGS" --cpu-type=O3CPU $GEM5_MEMORY
